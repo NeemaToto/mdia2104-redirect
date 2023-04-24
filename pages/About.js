2688adc80bdcff5eb1aa6fec1f1711c818bb4498
Neema Totonchi <ntotonchi1@se14l0120-15.ad.bcit.ca>
2023-04-24 14:20:18 -0700

    commit

---
+++ pages/About.js
@@ -0,0 +1,12 @@
+import styles from '@/styles/About.module.css'
+
+export default function About() {
+
+    return (
+        <>
+            <main style={styles.main}>
+                About Page
+            </main>
+        </>
+    )
+}
